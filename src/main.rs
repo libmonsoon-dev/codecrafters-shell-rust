5a71b9cbe02dd9fe5d636617638c2d46b653113d
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2025-12-24 11:38:32 +0300

    Handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command: String = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim())
 }

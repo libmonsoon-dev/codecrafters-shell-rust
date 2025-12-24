87c2b50c907af0274d96794a87faf0b17721a4b3
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2025-12-24 12:07:48 +0300

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    let mut shell = Shell::new();
+    shell.repl();
+}
 
-    let mut command: String = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim())
+struct Shell {
+    input: io::Stdin,
+    output: io::Stdout,
+    command: String,
+}
+
+impl Shell {
+    fn new() -> Shell {
+        Shell {
+            input: io::stdin(),
+            output: io::stdout(),
+            command: String::new(),
+        }
+    }
+
+    fn read(&mut self) -> io::Result<()> {
+        self.output.write_fmt(format_args!("$ "))?;
+        self.output.flush()?;
+        Ok(())
+    }
+
+    fn eval(&mut self) -> io::Result<()> {
+        self.input.read_line(&mut self.command)?;
+        Ok(())
+    }
+
+    fn print(&mut self) -> io::Result<()> {
+        self.output
+            .write_fmt(format_args!("{}: command not found\n", self.command.trim()))?;
+        Ok(())
+    }
+
+    fn repl(&mut self) {
+        loop {
+            self.read().unwrap();
+            self.eval().unwrap();
+            self.print().unwrap();
+        }
+    }
 }

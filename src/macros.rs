6034b0bf3eb2676d84e0ef565ae8fd20982860be
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2026-01-11 02:02:42 +0300

    CLI tests (cli-test-1768086162439)

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,25 @@
 #[macro_export]
+macro_rules! print_to {
+    ($out:expr, $fmt:expr) => {{
+        use std::io::Write;
+
+        $out.write_fmt(format_args!($fmt)).unwrap();
+    }};
+    ($out:expr, $fmt:expr, $($args:tt)*) => {{
+        use std::io::Write;
+
+        $out.write_fmt(format_args!($fmt, $($args)*)).unwrap();
+    }};
+}
+
+#[macro_export]
 macro_rules! print {
     ($fmt:expr) => {{
-        io::stdout().write_fmt(format_args!($fmt))?;
+        use std::io::Write;
+
+        crate::print_to!(std::io::stdout(), $fmt);
     }};
     ($fmt:expr, $($args:tt)*) => {{
-        io::stdout().write_fmt(format_args!($fmt, $($args)*))?;
+        crate::print_to!(std::io::stdout(), $fmt, $($args)*);
     }};
 }

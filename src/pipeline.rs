8c2fb8dfd4b955f5b26f823c4cf23a5c7be1a155
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2026-01-11 02:05:54 +0300

    Pipelines with built-ins

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,51 +1,53 @@
+use crate::bin_path::BinPath;
 use crate::parser::{Command, OutputStream};
-use std::thread::JoinHandle;
-use std::{io, process, thread};
+use crate::{print_to, BUILTIN_COMMANDS};
+use anyhow::bail;
+use std::cell::RefCell;
+use std::io::Write;
+use std::rc::Rc;
+use std::{env, fs, io, mem, process, thread};
 
 pub struct Pipeline<'a> {
     cmd: &'a Command,
-    threads: Vec<JoinHandle<()>>,
+    bin_path: Rc<RefCell<BinPath>>,
+    threads: Vec<thread::JoinHandle<()>>,
 }
 
 impl<'a> Pipeline<'a> {
-    pub fn new(cmd: &'a Command) -> Self {
+    pub fn new(cmd: &'a Command, bin_path: Rc<RefCell<BinPath>>) -> Self {
         Self {
             cmd,
+            bin_path,
             threads: Vec::with_capacity(4),
         }
     }
 
     pub fn run(&mut self) -> anyhow::Result<()> {
         let mut command = self.cmd;
-        let mut child = self.exec(&self.cmd.args, None)?;
-        let mut piped = false;
+        let mut process = self.call(&self.cmd.args, None)?;
+        // let mut piped = false;
 
         while let Some(output) = command.output() {
             let OutputStream::Pipe(pipe) = &output.to else {
                 break;
             };
 
-            piped = true;
-            let next_child =
-                self.exec(&pipe.args, child.stdout.take().map(process::Stdio::from))?;
-            self.wait_process(child);
+            // piped = true;
+            let next_process = self.call(&pipe.args, Some(process.stdout()))?;
+            process.wait(&mut self.threads);
 
             command = pipe;
-            child = next_child;
+            process = next_process;
         }
 
-        if !piped {
-            let child_stdin = child.stdin.take().expect("handle present");
-            self.copy_stdin(child_stdin, io::empty())
-        }
+        //TODO: remove
+        // if !piped {
+        //     self.copy_stdin(process.stdin(), io::empty())
+        // }
 
-        let child_stdout = child.stdout.take().expect("handle present");
-        self.copy_stdout(child_stdout, command.get_output()?);
-
-        let child_stderr = child.stderr.take().expect("handle present");
-        self.copy_stderr(child_stderr, command.get_error_output()?);
-
-        self.wait_process(child);
+        self.copy_stdout(process.stdout(), command.get_output()?);
+        self.copy_stderr(process.stderr(), command.get_error_output()?);
+        process.wait(&mut self.threads);
 
         for thread in self.threads.drain(..) {
             thread.join().unwrap();
@@ -54,63 +56,267 @@ impl<'a> Pipeline<'a> {
         Ok(())
     }
 
-    fn exec(
+    fn call(
         &mut self,
-        args: &Vec<String>,
-        stdin: Option<process::Stdio>,
-    ) -> anyhow::Result<process::Child> {
+        args: &'a Vec<String>,
+        stdin: Option<ProcessStdout>,
+    ) -> anyhow::Result<Box<dyn Process + 'a>> {
+        if BUILTIN_COMMANDS.contains(&&*args[0]) {
+            return Ok(Box::new(BuiltinProcess::new(
+                args,
+                Rc::clone(&self.bin_path),
+            )));
+        }
+
+        if let Some(_) = self.bin_path.borrow_mut().lookup(&args[0])? {
+            return Ok(Box::new(ExternalProcess::new(args, stdin)));
+        }
+
+        bail!("{}: command not found", args[0]);
+    }
+
+    //TODO: remove
+    // fn copy_stdin<T: io::Read + Send + 'static>(&mut self, stdin: ProcessStdin, mut input: T) {
+    //     let mut stdin: Box<dyn io::Write + Send + 'static> = match stdin {
+    //         ProcessStdin::ChildStdin(stdin) => Box::new(stdin),
+    //     };
+    //
+    //     let stdin_thread = thread::spawn(move || {
+    //         io::copy(&mut input, &mut stdin).unwrap();
+    //     });
+    //     self.threads.push(stdin_thread);
+    // }
+
+    fn copy_stdout<T: io::Write + Send + 'static>(&mut self, stdout: ProcessStdout, mut output: T) {
+        let mut stdout: Box<dyn io::Read + Send + 'static> = match stdout {
+            ProcessStdout::ChildStdout(stdout) => Box::new(stdout),
+            ProcessStdout::Buffer(buf) => Box::new(io::Cursor::new(buf)),
+        };
+
+        let stdout_thread = thread::spawn(move || {
+            io::copy(&mut stdout, &mut output).unwrap();
+        });
+        self.threads.push(stdout_thread);
+    }
+
+    fn copy_stderr<T: io::Write + Send + 'static>(&mut self, stderr: ProcessStderr, mut output: T) {
+        let mut stderr: Box<dyn io::Read + Send + 'static> = match stderr {
+            ProcessStderr::ChildStderr(stderr) => Box::new(stderr),
+            ProcessStderr::Buffer(buf) => Box::new(io::Cursor::new(buf)),
+        };
+
+        let stderr_thread = thread::spawn(move || {
+            io::copy(&mut stderr, &mut output).unwrap();
+        });
+        self.threads.push(stderr_thread);
+    }
+}
+
+trait Process {
+    // fn stdin(&mut self) -> ProcessStdin;
+
+    fn stdout(&mut self) -> ProcessStdout;
+
+    fn stderr(&mut self) -> ProcessStderr;
+
+    fn wait(&mut self, threads: &mut Vec<thread::JoinHandle<()>>);
+}
+
+// enum ProcessStdin {
+//     ChildStdin(process::ChildStdin),
+//     Buffer(Vec<u8>),
+// }
+
+enum ProcessStdout {
+    ChildStdout(process::ChildStdout),
+    Buffer(Vec<u8>),
+}
+
+enum ProcessStderr {
+    ChildStderr(process::ChildStderr),
+    Buffer(Vec<u8>),
+}
+
+struct BuiltinProcess<'a> {
+    args: &'a Vec<String>,
+    bin_path: Rc<RefCell<BinPath>>,
+    output: Vec<u8>,
+}
+
+impl<'a> BuiltinProcess<'a> {
+    fn new(args: &'a Vec<String>, bin_path: Rc<RefCell<BinPath>>) -> Self {
+        let mut p = Self {
+            args,
+            bin_path,
+            output: Vec::new(),
+        };
+
+        match p.args[0].as_ref() {
+            "exit" => process::exit(0),
+            "echo" => p.echo_builtin().unwrap(),
+            "type" => p.type_builtin().unwrap(),
+            "pwd" => print_to!(p.output, "{}\n", env::current_dir().unwrap().display()),
+            "cd" => p.cd_builtin().unwrap(),
+            _ => unimplemented!("builtin command {}", p.args[0]),
+        }
+
+        p
+    }
+
+    fn type_builtin(&mut self) -> io::Result<()> {
+        let _ = self.args.clone()[1..]
+            .iter()
+            .try_for_each(|arg| -> io::Result<()> {
+                if BUILTIN_COMMANDS.contains(&arg.as_str()) {
+                    print_to!(self.output, "{} is a shell builtin\n", arg);
+                    return Ok(());
+                }
+
+                if let Some(path) = self.bin_path.borrow_mut().lookup(&arg)? {
+                    print_to!(self.output, "{} is {}\n", arg, path.display());
+                    return Ok(());
+                }
+
+                print_to!(self.output, "{}: not found\n", arg);
+
+                Ok(())
+            })?;
+
+        Ok(())
+    }
+
+    fn cd_builtin(&mut self) -> io::Result<()> {
+        let path = if self.args.len() == 1 || self.args[1] == "~" {
+            env::var("HOME").unwrap()
+        } else {
+            self.args[1].clone()
+        };
+        let attr = fs::metadata(&path);
+        if matches!(attr, Err(ref err) if err.kind() == io::ErrorKind::NotFound) {
+            print_to!(self.output, "cd: {path}: No such file or directory\n");
+            return Ok(());
+        }
+
+        env::set_current_dir(&path)?;
+
+        Ok(())
+    }
+
+    fn echo_builtin(&mut self) -> io::Result<()> {
+        let str = self.args[1..].join(" ");
+        print_to!(self.output, "{str}\n");
+
+        Ok(())
+    }
+}
+
+impl<'a> Process for BuiltinProcess<'a> {
+    // fn stdin(&mut self) -> ProcessStdin {
+    //     todo!()
+    // }
+
+    fn stdout(&mut self) -> ProcessStdout {
+        ProcessStdout::Buffer(mem::take(&mut self.output))
+    }
+
+    fn stderr(&mut self) -> ProcessStderr {
+        ProcessStderr::Buffer(Vec::new())
+    }
+
+    fn wait(&mut self, _threads: &mut Vec<thread::JoinHandle<()>>) {
+        // Noop
+    }
+}
+
+struct ExternalProcess {
+    stdin_buf: Option<Vec<u8>>,
+    child: Option<process::Child>,
+}
+
+impl<'a> ExternalProcess {
+    fn new(args: &'a Vec<String>, stdin: Option<ProcessStdout>) -> Self {
         let mut cmd = process::Command::new(&args[0]);
 
         args[1..].iter().for_each(|arg| {
             cmd.arg(arg);
         });
 
+        let mut stdin_buf = None;
+        let stdin = stdin
+            .and_then(|stdin| match stdin {
+                ProcessStdout::ChildStdout(child) => Some(process::Stdio::from(child)),
+                ProcessStdout::Buffer(buf) => {
+                    stdin_buf = Some(buf);
+                    None
+                }
+            })
+            .unwrap_or(process::Stdio::piped());
+
         let child = cmd
-            .stdin(stdin.unwrap_or(process::Stdio::piped()))
+            .stdin(stdin)
             .stdout(process::Stdio::piped())
             .stderr(process::Stdio::piped())
-            .spawn()?;
+            .spawn()
+            .unwrap();
 
-        Ok(child)
+        Self {
+            stdin_buf,
+            child: Some(child),
+        }
+    }
+}
+
+impl Process for ExternalProcess {
+    // fn stdin(&mut self) -> ProcessStdin {
+    //     ProcessStdin::ChildStdin(
+    //         self.child
+    //             .as_mut()
+    //             .unwrap()
+    //             .stdin
+    //             .take()
+    //             .expect("handle present"),
+    //     )
+    // }
+
+    fn stdout(&mut self) -> ProcessStdout {
+        ProcessStdout::ChildStdout(
+            self.child
+                .as_mut()
+                .unwrap()
+                .stdout
+                .take()
+                .expect("handle present"),
+        )
     }
 
-    fn copy_stdin<T: io::Read + Send + 'static>(
-        &mut self,
-        mut child_stdin: process::ChildStdin,
-        mut input: T,
-    ) {
-        let stdin_thread = thread::spawn(move || {
-            io::copy(&mut input, &mut child_stdin).unwrap();
-        });
-        self.threads.push(stdin_thread);
+    fn stderr(&mut self) -> ProcessStderr {
+        ProcessStderr::ChildStderr(
+            self.child
+                .as_mut()
+                .unwrap()
+                .stderr
+                .take()
+                .expect("handle present"),
+        )
     }
 
-    fn copy_stdout<T: io::Write + Send + 'static>(
-        &mut self,
-        mut child_stdout: process::ChildStdout,
-        mut output: T,
-    ) {
-        let stdout_thread = thread::spawn(move || {
-            io::copy(&mut child_stdout, &mut output).unwrap();
-        });
-        self.threads.push(stdout_thread);
-    }
+    fn wait(&mut self, threads: &mut Vec<thread::JoinHandle<()>>) {
+        let mut child = mem::take(&mut self.child).unwrap();
 
-    fn copy_stderr<T: io::Write + Send + 'static>(
-        &mut self,
-        mut child_stderr: process::ChildStderr,
-        mut output: T,
-    ) {
-        let stderr_thread = thread::spawn(move || {
-            io::copy(&mut child_stderr, &mut output).unwrap();
-        });
-        self.threads.push(stderr_thread);
-    }
+        match self.stdin_buf {
+            Some(ref mut buf) => child
+                .stdin
+                .take()
+                .expect("handle present")
+                .write_all(buf)
+                .unwrap(),
+            None => {}
+        }
 
-    fn wait_process(&mut self, mut child: process::Child) {
         let process = thread::spawn(move || {
             child.wait().unwrap();
         });
-        self.threads.push(process);
+
+        threads.push(process);
     }
 }
